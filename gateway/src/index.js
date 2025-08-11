c411c74813a1e665e9a62a0c1aeb2ef77beaa96f
silver1556 <mamunmunshi1556@gmail.com>
2025-08-11 21:43:23 +0800

    Update index.js

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.GATEWAY_PORT || 3000;
 
 // Configure logger
 const logger = winston.createLogger({
